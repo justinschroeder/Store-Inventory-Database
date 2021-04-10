0a6db186388e965ee6742168bb8ca44a10b38bdf
Justin Schroeder <justin.schroeder3@gmail.com>
2021-04-10 16:52:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,57 @@
 from sqlalchemy.sql.coercions import expect_col_expression_collection
 from sqlalchemy.sql.expression import column
 from models import (Base, session, Product, engine)
+from collections import OrderedDict
 import csv
 import datetime
 import time
-from collections import OrderedDict
+import os
 
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def menu_loop():
+    """SHOW MENU"""
+    choice = None
+
+    while choice != 'q':
+        clear()
+        print('Enter "q" to quit.')
+        for key, value in menu.items():
+            print(f'{key}) {value.__doc__}')
+        choice = input('Action: ').lower().strip()
+
+        if choice in menu:
+            clear()
+            menu[choice]()
+        else:
+            print('\nThat is not a valid option.\nPlease try again.')
+            time.sleep(2)
+
+
+def view_product():
+    """VIEW ENTRY BY ID"""
+    pass
+
+
+def add_product():
+    """ADD PRODUCT TO INVENTORY"""
+    pass
+
+
+def backup():
+    """BACKUP INVENTORY"""
+    pass
+
+
+menu = OrderedDict([
+    ('a', add_product),
+    ('v', view_product),
+    ('b', backup),
+])
 
 if __name__== '__main__':
     Base.metadata.create_all(engine)
-    
+    menu_loop()
